92d6969f5e94598a06dfd3a10eaa3d79e8f6b887
Matzaa Wahorn <matzaa@matzaa.com>
2020-12-15 13:42:48 +0100

    legend axis added

--- script.js
+++ script.js
@@ -31,10 +31,17 @@
         // colors
 
         const colors = [
-            "rgb(115, 230, 186)",
-            "rgb(231, 223, 111)",
-            "rgb(230, 180, 115)",
-            "rgb(231, 94, 94)",
+            // "rgb(115, 230, 186)",
+            // // "rgb(255, 123, 123)",
+            // "rgb(231, 223, 111)",
+            // "rgb(230, 180, 115)",
+            // "rgb(231, 94, 94)",
+            "#f8d1c9",
+            "#d2a5a8",
+            "#a58f9b",
+            "#6a768b",
+            "#50637c",
+            "#334961",
         ];
 
         const colorScale = d3
@@ -47,34 +54,44 @@
 
         // legend
 
+        const legendSquareW = 40;
         console.log(
             "minimum",
             d3.max(userEdu, (d) => d.bachelorsOrHigher)
         );
+        console.log("colorscale: ", colorScale(0));
+
         const x = d3
             .scaleLinear()
             .domain([
                 d3.min(userEdu, (d) => d.bachelorsOrHigher),
                 d3.max(userEdu, (d) => d.bachelorsOrHigher),
             ])
-            .range([0, 100]);
+            .range([0, legendSquareW * 6]);
 
         const legend = base
             .append("g")
             .attr("id", "legend")
             .attr("width", 300)
-            .attr("height", 80);
+            .attr("height", 80)
+            .attr("transform", "translate(830, 420)");
 
-        const legendSquareW = 40;
         legend
             .selectAll("rect")
             .data(colors)
             .enter()
             .append("rect")
-            .attr("x", (d, i) => i * legendSquareW + legendSquareW / 2)
+            .attr("x", (d, i) => i * legendSquareW)
             .attr("fill", (d) => d)
             .attr("width", legendSquareW)
             .attr("height", 30);
+
+        const legendAxis = d3.axisBottom(x);
+
+        legend
+            .append("g")
+            .attr("transform", "translate(0, 30)")
+            .call(legendAxis);
 
         // map
 
@@ -91,13 +108,15 @@
             .attr("fill", function (d) {
                 let result = userEdu.filter((el) => el.fips === d.id);
                 // console.log("result: ", result);
-                if (result[0].bachelorsOrHigher < 20) {
+                if (result[0].bachelorsOrHigher <= colorScale(1)) {
                     return colors[0];
-                } else if (result[0].bachelorsOrHigher < 40) {
+                } else if (result[0].bachelorsOrHigher <= colorScale(2)) {
                     return colors[1];
-                } else if (result[0].bachelorsOrHigher < 60) {
+                } else if (result[0].bachelorsOrHigher <= colorScale(3)) {
                     return colors[2];
-                } else if (result[0].bachelorsOrHigher < 80) {
+                } else if (result[0].bachelorsOrHigher <= colorScale(4)) {
+                    return colors[3];
+                } else if (result[0].bachelorsOrHigher <= colorScale(5)) {
                     return colors[3];
                 }
             })
